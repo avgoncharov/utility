84c9621b6963f554bcc2283d6bd7488dfd4f8a51
Andrey Goncharov <a.v.goncharov@voskhod.ru>
2015-08-07 17:19:57 +0300

    Edit. Took in account, that line consist of id and value, which are separated by tab (/t).

--- py/txt2sql/txt2sql.py
+++ py/txt2sql/txt2sql.py
@@ -1,9 +1,11 @@
-# Input file consist of lines. Values in every line are separated by whitespace.
+# Input file consist of lines. Each line consist of id and some value, which are separated by tab (\t).
 with open('input.txt') as in_f:
     lines = in_f.readlines()
 
 # Input file convert to sql-script. Every line of the input file is converted to insert command of output script.
 with open('output.sql','w') as out_f:
     for itr in lines:
-        buf = itr.split()
-        out_f.write("INSERT INTO TableName (ClmnA, ClmnB) VALUES ('{0}', '{1}');\n".format(buf[0], buf[1]))
+        i = itr.index('\t')
+        b1 = itr[:i]
+        b2 = itr[i+1:len(itr) - 1]
+        out_f.write("INSERT INTO TableName (ClmnA, ClmnB) VALUES ('{0}', '{1}');\n".format(b1, b2))
